b8eef9649faa03c83ea66eee11b69e663120c67b
Pranay1906 <65688346+Pranay1906@users.noreply.github.com>
2020-12-20 02:06:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let screen = document.getElementById('screen');
+let button = document.querySelectorAll('button');
+let screenValue = '';
+for (item of button) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log("this is press", buttonText)
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        } else if (buttonText == "C") {
+            screenValue = '';
+            screen.value = screenValue;
+        } else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+
+        } else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
